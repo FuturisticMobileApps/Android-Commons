4a753ab4f1a6c91a659dfa1adc22b077913bda87
Subramanian S <stssubramanians@spantechnology.net>
2024-03-11 15:16:43 +0530

    Init commit

---
+++ samples/src/main/java/com/futuristicmobilieapps/androidcommons/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.futuristicmobilieapps.androidcommons
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample_activity)
+    }
+}
