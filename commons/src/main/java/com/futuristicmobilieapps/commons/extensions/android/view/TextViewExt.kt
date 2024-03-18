ba9e5f7532d4b2e7ff70c83c666acef4d12e0c7f
STS820- Nizar Ahmed <nizar.ahmed@spantechnologyservices.com>
2024-03-18 15:59:29 +0530

    first TextView extension

--- commons/src/main/java/com/futuristicmobilieapps/commons/extensions/android/view/TextViewExt.kt
+++ commons/src/main/java/com/futuristicmobilieapps/commons/extensions/android/view/TextViewExt.kt
@@ -1,2 +1,8 @@
 package com.futuristicmobilieapps.commons.extensions.android.view
 
+import android.widget.TextView
+
+fun TextView?.clearText() {
+    if (this?.text.isNullOrEmpty()) return
+    else this!!.text = ""
+}
