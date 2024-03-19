948b2747807fa1756f45306d40f49a3ad0356bb2
REENA J <reena.j@spantechnologyservices.com>
2024-03-19 15:41:11 +0530

    commit

--- commons/src/main/java/com/futuristicmobilieapps/commons/extensions/android/fields/DateExt.kt
+++ commons/src/main/java/com/futuristicmobilieapps/commons/extensions/android/fields/DateExt.kt
@@ -1,2 +1,21 @@
 package com.futuristicmobilieapps.commons.extensions.android.fields
 
+import com.futuristicmobilieapps.commons.extensions.kotlin.validateString
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+
+fun SimpleDateFormat.getDateFromMillis(timeInMillis: Long) = format(timeInMillis).validateString()
+
+fun getCurrentYear(): Int = Calendar.getInstance().get(Calendar.YEAR)
+
+fun getCurrentMonth(): Int = Calendar.getInstance().get(Calendar.MONTH)
+
+fun SimpleDateFormat.getCurrentDate(): String = format(Date())
+
+fun SimpleDateFormat.getCurrentTimeInMS(additionalTime: Long = 0): Date? {
+
+    val currentTime = System.currentTimeMillis() + additionalTime
+
+    return parse(format(currentTime))
+}
--- commons/src/main/java/com/futuristicmobilieapps/commons/extensions/kotlin/StringExt.kt
+++ commons/src/main/java/com/futuristicmobilieapps/commons/extensions/kotlin/StringExt.kt
@@ -3,32 +3,28 @@ package com.futuristicmobilieapps.commons.extensions.kotlin
 import java.text.NumberFormat
 import java.util.Locale
 
-fun CharSequence?.validateString(): String {
-    return this?.toString()?.trim() ?: ""
-}
-fun String?.validateLength(): Int {
-    return validateString().length
-}
+fun CharSequence?.validateString() = this?.toString()?.trim() ?: ""
 
-fun String?.validateString(): String {
-    return this?.trim() ?: ""
-}
+fun String?.validateLength() = validateString().length
 
 fun CharSequence?.isValidString(): Boolean {
+
     val textValue = this?.toString()?.trim() ?: return false
+
     return textValue.isNotEmpty() && !textValue.equals("null", ignoreCase = true)
 }
 
 fun String.convertToDollarFormat(): String =
-    NumberFormat.getCurrencyInstance(Locale.US).format(validateString().toDoubleOrNull().validateDouble())
+
+    NumberFormat.getCurrencyInstance(Locale.US).format(validateString()
+        .toDoubleOrNull().validateDouble())
+
 
 fun String?.getAmountValueFromDollarFormat(): Double {
-    if (this.isNullOrEmpty() || !isValidString()) return 0.00
 
-    return try {
-        NumberFormat.getCurrencyInstance(Locale.US).parse(this)?.toDouble() ?: 0.00
-    } catch (e: Exception) {
-        this?.replace(Regex("[$,]"), "")?.trim()?.toDoubleOrNull() ?: 0.00
-    }
+    return NumberFormat.getCurrencyInstance(Locale.US)
+        .parse(this.validateString())?.toDouble() ?: this?.validateString()
+        ?.replace(Regex("[$,]"), "")?.toDoubleOrNull() ?: 0.00
 }
+
 
