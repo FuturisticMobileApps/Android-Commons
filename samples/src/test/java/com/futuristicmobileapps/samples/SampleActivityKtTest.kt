b0c41747eb99c01378f34fd0d3e935441c1fdb10
Subramanian S <stssubramanians@spantechnology.net>
2024-03-26 16:37:01 +0530

    Recommit

--- samples/src/test/java/com/futuristicmobileapps/samples/SampleActivityKtTest.kt
+++ samples/src/test/java/com/futuristicmobileapps/samples/SampleActivityKtTest.kt
@@ -6,7 +6,9 @@ import android.widget.EditText
 import com.futuristicmobilieapps.commons.extensions.android.fields.convertZipCode
 import com.nhaarman.mockitokotlin2.argumentCaptor
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 
 
 class EditTextTest {
@@ -14,24 +16,24 @@ class EditTextTest {
     @Test
     fun `test zip code is formatted correctly`() {
 
-        val editText = Mockito.mock(EditText::class.java)
-        val editable = Mockito.mock(Editable::class.java)
+        val editText = mock(EditText::class.java)
+        val editable = mock(Editable::class.java)
         val textWatcherCaptor = argumentCaptor<TextWatcher>()
 
         val inputZipCode = "123456789"
         val expectedZipCode = "12345-6789"
 
-        Mockito.`when`(editable.toString()).thenReturn(inputZipCode)
+        `when`(editable.toString()).thenReturn(inputZipCode)
 
-        Mockito.`when`(editText.text).thenReturn(editable)
+        `when`(editText.text).thenReturn(editable)
 
         editText.convertZipCode()
 
-        Mockito.verify(editText).addTextChangedListener(textWatcherCaptor.capture())
+        verify(editText).addTextChangedListener(textWatcherCaptor.capture())
         val textWatcher = textWatcherCaptor.firstValue
         textWatcher.onTextChanged(editable, 0, 0, 0)
 
-        Mockito.verify(editText).setText(expectedZipCode)
+        verify(editText).setText(expectedZipCode)
 
     }
 
